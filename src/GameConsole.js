2aee881379104aa7b8fa9c1227349add451a644e
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 10:04:24 +0300

    Rokas

--- src/GameConsole.js
+++ src/GameConsole.js
@@ -38,7 +38,11 @@ export default class Game {
         for(const combination of winningCombinations) {
             const [a, b, c] = combination;
 
-
+            if(this.board[a]) {
+                if (this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+                    return combination;
+                }
+            }
         }
     }
 
