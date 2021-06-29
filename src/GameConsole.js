dd3a657d54341bcb09a91a1af195fb8e8dbc4a76
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 09:54:36 +0300

    Rokas

--- src/GameConsole.js
+++ src/GameConsole.js
@@ -16,8 +16,23 @@ export default class Game {
     makeMove(i) {
         // and puts something in here
         // defines the position
+        if(this.board[i]) {
+            return;
+        }
         this.board[i] = this.turn;
+    }
 
+    findWinningCombination() {
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [6,4,2],
+        ]
     }
 
 }
